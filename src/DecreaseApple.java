fea7075f4cd468a55a476632f3ce9d84dbfab565
pedrok <pedrohlima20@gmail.com>
2019-11-10 14:45:11 -0300

    Criação da Decrease Fruit.

---
+++ src/DecreaseApple.java
@@ -0,0 +1,24 @@
+import java.awt.Point;
+import java.util.LinkedList;
+
+import javax.swing.ImageIcon;
+
+@SuppressWarnings("serial")
+public class DecreaseApple extends Apple {
+	private ImageIcon appleicon = new ImageIcon("C:\\Users\\Pichau\\Desktop\\Triangulo\\T_DOWN.png");
+	
+	
+	public DecreaseApple() {
+		super();
+		setIcon(appleicon);
+		setLocation(400, 400);
+	}
+	
+	public void collision(Snake s,  LinkedList<Point> pontos) {
+		if ((s.getX() == this.getX()) && (s.getY() == this.getY())) {
+			pontos.subList(3, pontos.size()).clear();
+			setLocation(-10,  -10);
+		}
+	}
+	
+}
