3a5ac5de37f7f4c11b7899ffade2cb145d12cd14
pedrok <pedrohlima20@gmail.com>
2019-11-10 14:41:09 -0300

    Criação da Big fruit

---
+++ src/BigApple.java
@@ -0,0 +1,25 @@
+import java.awt.Point;
+import java.util.LinkedList;
+
+import javax.swing.ImageIcon;
+
+@SuppressWarnings("serial")
+public class BigApple extends Apple {
+	private ImageIcon appleicon = new ImageIcon("C:\\Users\\Pichau\\Desktop\\Triangulo\\BIG.png");
+	
+	
+	public BigApple() {
+		super();
+		setIcon(appleicon);
+		setLocation(400, 400);
+	}
+	
+	public void collision(Snake s,  LinkedList<Point> pontos) {
+		if ((s.getX() == this.getX()) && (s.getY() == this.getY())) {
+			pontos.addLast(new Point(pontos.get(pontos.size() - 1)));
+			s.score+=2;
+			setLocation(-10,  -10);
+		}
+	}
+	
+}
