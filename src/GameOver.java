123406f310b9817fd1435b760417533be5630a71
pedrok <pedrohlima20@gmail.com>
2019-11-10 14:51:57 -0300

    Pequenas atualizaçoes na janela de gameover

--- src/GameOver.java
+++ src/GameOver.java
@@ -2,6 +2,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 
+@SuppressWarnings("serial")
 public class GameOver extends JLabel {
 
 	ImageIcon gameover = new ImageIcon("C:\\Users\\Pichau\\Desktop\\Triangulo\\GAMEOVER.png");
@@ -9,9 +10,7 @@ public class GameOver extends JLabel {
 	public GameOver () {
 		setSize(300, 300);
 		setLayout(null);
-		
 		setVisible(true);
-		
 	}
 	
 }
