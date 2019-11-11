2f6565007f99d6bcc77715675d3f3fecad6295f0
pedrok <pedrohlima20@gmail.com>
2019-11-11 16:50:41 -0300

    Criação de gameover panel

--- src/GameOver.java
+++ src/GameOver.java
@@ -1,16 +1,25 @@
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 @SuppressWarnings("serial")
-public class GameOver extends JLabel {
+public class GameOver extends JPanel {
 
-	ImageIcon gameover = new ImageIcon("C:\\Users\\Pichau\\Desktop\\Triangulo\\GAMEOVER.png");
+	JLabel go = new JLabel();
 	
 	public GameOver () {
 		setSize(300, 300);
 		setLayout(null);
 		setVisible(true);
+		go.setText("GameOver");
+		go.setHorizontalAlignment(JLabel.CENTER);
+		go.setSize(180, 90);
+		go.setLocation(60, 60);
+		go.setVisible(true);
+		add(go);
+		
+		
+		
 	}
 	
 }
