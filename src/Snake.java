1fdacde96178cc133da10446668da52c3eb704ac
pedrok <pedrohlima20@gmail.com>
2019-11-11 16:57:00 -0300

    Atualizaç

--- src/Snake.java
+++ src/Snake.java
@@ -8,7 +8,8 @@ import javax.swing.JLabel;
 public class Snake extends JLabel {
 
 	private int x = 30, y = 30, larg = 10, alt = 10;
-	protected int score=0;
+	protected int score = 0;
+	protected boolean doublescore=false;
 
 	ImageIcon head = new ImageIcon("C:\\Users\\Pichau\\Desktop\\Triangulo\\SQHead.png");
 
@@ -18,14 +19,39 @@ public class Snake extends JLabel {
 		setIcon(head);
 	}
 
-	public boolean MoveAndCheck(Snake s, int VX, int VY, LinkedList<Point> pontos) {
+	//codigo para fazer a cobra morrer ao bater na parede
+	
+	/*
+	 * public boolean MoveAndCheck(Snake s, int VX, int VY, LinkedList<Point>
+	 * pontos) { x = x + VX; y = y + VY; if (x < 0 || x > 280 || y < 0 || y > 260) {
+	 * return false; } for (int i = 1; i < pontos.size(); i++) { if
+	 * (pontos.get(i).getX() == x && pontos.get(i).getY() == y) { return false; } }
+	 * s.setLocation(x, y); return true; }
+	 */
+
+	public boolean MoveAndCheck(Snake s, int VX, int VY, LinkedList<Point> pontos, LinkedList<Point> wall) {
 		x = x + VX;
 		y = y + VY;
-		if (x < 0 || x > 280 || y < 0 || y > 260) {
-			return false;
+		if (x < 0) {
+			x = 280;
 		}
+		if (x > 280) {
+			x=0;
+		}
+		if (y < 0) {
+			y=260;
+		}
+		if (y > 260) {
+			y=0;
+		}
+
 		for (int i = 1; i < pontos.size(); i++) {
 			if (pontos.get(i).getX() == x && pontos.get(i).getY() == y) {
+				return false;
+			}
+		}
+		for (int i = 0; i < wall.size(); i++) {
+			if (wall.get(i).getX() == x && wall.get(i).getY() == y) {
 				return false;
 			}
 		}
@@ -38,8 +64,8 @@ public class Snake extends JLabel {
 			pontos.addLast(new Point(pontos.get(pontos.size() - 1)));
 			a.generateApple();
 			score++;
-			System.out.println("Score = " +score);
-			
+			System.out.println("Score = " + score);
+
 		}
 	}
 }
