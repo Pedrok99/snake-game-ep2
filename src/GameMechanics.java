ec3c379320dfe4338ddcd6b318c7b7e23415e024
pedrok <pedrohlima20@gmail.com>
2019-11-12 01:20:01 -0300

    Adição final do GameOver Panel e limpeza de metodos.

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -2,60 +2,79 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class GameMechanics extends JPanel implements Runnable, KeyListener {
+public class GameMechanics extends JPanel implements Runnable, KeyListener, ActionListener {
 
-	private JFrame display = new JFrame();
-	private Thread thread = new Thread(this);
+	protected JFrame display = new JFrame();
+	private Thread thread;
 	private JLabel end = new JLabel();
+	private JLabel scoreLabel = new JLabel();
 	private GameOver gameoverpanel = new GameOver();
-
-	//private Snake S1 = new Snake();
-
-	 private KittySnake S1 = new KittySnake();
-
-	 //private StarSnake S1 = new StarSnake();
-
+	private Snake S1;
+	protected boolean finalflag = false;
+	private JButton ok = new JButton("OK");
 	private Apple A1 = new Apple();
-	private int Xspeed = 10, Yspeed = 0, i;
-	private boolean isRunning = true;
+	protected int Xspeed = 10, Yspeed = 0, i, check = 1, snaketype;
+	protected boolean isRunning = true;
 	private LinkedList<Point> Pontos = new LinkedList<Point>();
 	private Point lastp = new Point();
-//	private ImageIcon img = new ImageIcon("C:\\Users\\Pichau\\Desktop\\Triangulo\\GAMEOVER.png");
 	private LinkedList<Point> wall = new LinkedList<Point>();
 	private Point aux = new Point();
 	private BombApple bomba = new BombApple();
 	private BigApple big = new BigApple();
 	private DecreaseApple Decrease = new DecreaseApple();
-	private SpecialSpawner spawner = new SpecialSpawner(bomba, big, Decrease);// -------------
+	private SpecialSpawner spawner = new SpecialSpawner(bomba, big, Decrease);
 
-	public GameMechanics() {
+	public GameMechanics(int st) {
+		if (st == 1) {
+			S1 = new Snake();
+		} else if (st == 2) {
+			S1 = new KittySnake();
+		} else if (st == 3) {
+			S1 = new StarSnake();
+		}
 		cfgDisplay();
+		ok.setBounds(101, 200, 90, 30);
+		ok.setVisible(true);
+		ok.addActionListener(this);
+		ok.setBackground(Color.DARK_GRAY);
+		ok.setFont(ok.getFont().deriveFont(16.0f));
+		ok.setForeground(Color.ORANGE);
+		ok.setFocusPainted(false);
+		ok.setBorderPainted(false);
+		gameoverpanel.add(ok);
+		thread = new Thread(this);
 		setSize(300, 300);
 		setLocation(0, 0);
 		setLayout(null);
 		setVisible(true);
-		setBackground(Color.lightGray);
+		setBackground(Color.gray);
 		add(S1);
 		add(A1);
-
+		add(scoreLabel);
 		add(bomba);// -------------
 		add(big);
 		add(Decrease);
-		display.add(gameoverpanel);
+		end.setSize(300, 300);
+		end.setLocation(0, 0);
+		end.setLayout(null);
+		end.setVisible(false);
 
-		//Pontos.add(new Point(20, 30));
-		//Pontos.add(new Point(10, 30));
-		//Pontos.add(new Point(0, 30));
+		Pontos.add(new Point(20, 30));
+		Pontos.add(new Point(10, 30));
+		Pontos.add(new Point(0, 30));
 		thread.start();
 
 	}
@@ -72,11 +91,18 @@ public class GameMechanics extends JPanel implements Runnable, KeyListener {
 		display.add(this);
 		display.add(end);
 		display.setLocationRelativeTo(null);
+
 		end.setSize(300, 300);
 		end.setLocation(0, 0);
 		end.setLayout(null);
 		end.setVisible(false);
-		// end.setIcon(img);
+
+		scoreLabel.setSize(80, 15);
+		scoreLabel.setLocation(120, 0);
+		scoreLabel.setLayout(null);
+		scoreLabel.setVisible(true);
+		scoreLabel.setForeground(Color.green);
+
 		barrier();
 		display.pack();
 
@@ -113,6 +139,8 @@ public class GameMechanics extends JPanel implements Runnable, KeyListener {
 			Yspeed = 0;
 			Xspeed = 10;
 			break;
+		default:
+			break;
 		}
 
 	}
@@ -128,15 +156,10 @@ public class GameMechanics extends JPanel implements Runnable, KeyListener {
 	@Override
 
 	public void run() {
-		Pontos.add(new Point(20, 30));
-		Pontos.add(new Point(10, 30));
-		Pontos.add(new Point(0, 30));
-
 		while (isRunning) {
 
 			lastp.setLocation(S1.getLocation());
 			isRunning = S1.MoveAndCheck(S1, Xspeed, Yspeed, Pontos, wall); // define as proximas cords da cobra
-
 			if (!isRunning) {
 				break;
 			}
@@ -145,9 +168,10 @@ public class GameMechanics extends JPanel implements Runnable, KeyListener {
 				break;
 			}
 			spawner.generateSpecial();// -------------
-			//bomba.collision(S1);
 			big.collision(S1, Pontos);
 			Decrease.collision(S1, Pontos);
+			scoreLabel.setText("Score : " + S1.score);
+			// display.setTitle("Score : "+S1.score);
 			S1.AppleCollision(S1, A1, Pontos);
 			repaint();
 			try {
@@ -155,24 +179,29 @@ public class GameMechanics extends JPanel implements Runnable, KeyListener {
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-
 			}
-		}
 
-		endscreen();
+		}
+		display.add(gameoverpanel);
+		gameoverpanel.setVisible(true);
+		setVisible(false);
+		scoreLabel.setLocation(115, 130);
+		scoreLabel.setSize(80, 15);
+		scoreLabel.setFont(scoreLabel.getFont().deriveFont(16.0f));
+		gameoverpanel.add(scoreLabel);
+		
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		try{
-		//System.out.println("Tamanho da lista: "+Pontos.size());
-		aux = Pontos.removeLast();
-		Pontos.addFirst(aux);
-		Pontos.get(0).setLocation(lastp);
-		}catch(NoSuchElementException e){
-			System.out.println("POKEBOLAAAAAAAA VAAAAAAAAAAAAAAAAAAAAAAAAAIII");
+
+		try {
+			aux = Pontos.removeLast();
+			Pontos.addFirst(aux);
+			Pontos.get(0).setLocation(lastp);
+		} catch (NoSuchElementException e) {
+			// ??
 		}
 		for (i = 0; i < Pontos.size(); i++) {
 			g.fillOval(Pontos.get(i).x, Pontos.get(i).y, 10, 10);
@@ -198,4 +227,9 @@ public class GameMechanics extends JPanel implements Runnable, KeyListener {
 		}
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		finalflag = true;
+
+	}
 }
